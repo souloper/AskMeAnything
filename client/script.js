2cbf56d74966840213ff0f28fedda19206640a02
souloper <cse2019026@rcciit.org.in>
2022-12-26 22:33:10 +0530

    script.js update

--- client/script.js
+++ client/script.js
@@ -92,7 +92,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch("https://askmeanything.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
